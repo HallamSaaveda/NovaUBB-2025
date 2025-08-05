8a7e496ea5e8fc1e19bca643518ddbc7a560b96e
Felipe Miranda <felipe.miranda2001@alumnos.ubiobio.cl>
2025-08-04 23:14:56 -0400

    se agregan rutas de algoritmos al index.routes

--- backend/src/routes/index.routes.js
+++ backend/src/routes/index.routes.js
@@ -4,6 +4,8 @@ import userRoutes from "./user.routes.js"
 import investigacionRoutes from "./investigacion.routes.js"
 import archivoPersonalRoutes from "./archivo-personal.routes.js" //
 import permutacionesRoutes from "./permutaciones.routes.js"
+import secuenciaAlineamiento from "./aligment.routes.js"
+import buscarPermutacion from "./searchPermutation.routes.js"
 
 const router = Router()
 
@@ -12,5 +14,7 @@ router.use("/users", userRoutes)
 router.use("/investigaciones", investigacionRoutes)
 router.use("/personal", archivoPersonalRoutes) //
 router.use("/permutaciones", permutacionesRoutes)
+router.use("/alineamiento", secuenciaAlineamiento)
+router.use("/buscar-permutacion", buscarPermutacion)
 
 export default router
